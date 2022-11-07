a378fdb7703c74c6eb96c0c7aa575d727afb91d2
Sawan <itissaavn@gmail.com>
2022-11-07 12:25:26 +0530

    add enhanced object literal

--- data-st-ops/starter/script.js
+++ data-st-ops/starter/script.js
@@ -4,57 +4,72 @@
 // const flights =
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// // Data needed for first part of the section
-// const restaurant = {
-//   name: 'Classico Italiano',
-//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// Data needed for first part of the section
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-//   order: function (starterIndex, mainIndex) {
-//     return [this.starterMeun[starterIndex], this.mainMenu[mainIndex]];
-//   },
+  //openingHours : openingHours //old way
 
-//   orderDelivery: function ({
-//     starterIndex = 1,
-//     mainIndex = 0,
-//     time = '20:00',
-//     address,
-//   }) {
-//     // defining  default values for the parameters
-//     console.log(
-//       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-//     );
-//   },
+  //new way - ES6 enhanced object literal
+  openingHours,
 
-//   orderPasta: function (ing1, ing2, ing3) {
-//     console.log(
-//       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
-//     );
-//   },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMeun[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-//   orderPizza: function (mainIngredient, ...otherIngredients) {
-//     //rest pattern - collects all the remaining elements into an array like otherIngredients will be an array
-//     console.log(mainIngredient);
-//     console.log(otherIngredients);
-//   },
-// };
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    // defining  default values for the parameters
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    //rest pattern - collects all the remaining elements into an array like otherIngredients will be an array
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+//for of loop
+for (const item of menu) console.log(item);
+
+for (const [index, item] of menu.entries()) {
+  console.log(`${index}: ${item}`);
+}
 
 // restaurant.orderDelivery({
 //   time: '22:30',
@@ -316,34 +331,35 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 
 GOOD LUCK 😀
 */
-//1
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// //1
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-//2
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
+// //2
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
 
-//3
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
+// //3
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
 
-//4
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// //4
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
 
-//5
-const { team1, x: draw, team2 } = game.odds;
-console.log(team1, team2, draw);
+// //5
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, team2, draw);
 
-//6
-function printGoals(...playerNames) {
-  console.log(playerNames);
-  console.log(`${playerNames.length} goals were scored.`); // when gets scoring player as input
-}
-printGoals(...game.scored);
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// //6
+// function printGoals(...playerNames) {
+//   console.log(playerNames);
+//   console.log(`${playerNames.length} goals were scored.`); // when gets scoring player as input
+// }
+// printGoals(...game.scored);
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
 
-//7
-const winner = team1 < team2 && 'team 2 will win';
-console.log(winner);
+// //7
+// // when team1> team 2 is true
+// const winner = team1 > team2 && 'team 1 will win';
+// console.log(winner);
